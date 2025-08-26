b30fdc27eea0d463e51f71aa4a14eded3d436b2c
ariaugustoo <ariaugustoo@gmail.com>
2025-08-26 18:06:20 -0300

    main.py

--- main.py
+++ main.py
@@ -1,25 +1,19 @@
+import os
 import logging
 from flask import Flask, request
-from telegram import Bot, Update
-from processador import (
-    processar_comprovante,
-    marcar_como_pago,
-    total_pendentes,
-    listar_pendentes,
-    listar_pagos,
-    ajuda,
-    ultimo_comprovante,
-    total_geral
-)
+import telegram
+from processador import processar_mensagem
 
 TOKEN = "8044957045:AAE8AmsmV3LYwqPUi6BXmp_I9ePgywg8OIA"
 GROUP_ID = -1002626449000
 
-bot = Bot(token=TOKEN)
+bot = telegram.Bot(token=TOKEN)
 app = Flask(__name__)
 
-# Logs
-logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
+# Ativa logs para debug
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
 logger = logging.getLogger(__name__)
 
 @app.route("/")
@@ -29,41 +23,13 @@ def home():
 @app.route(f"/{TOKEN}", methods=["POST"])
 def webhook():
     try:
-        update = Update.de_json(request.get_json(force=True), bot)
-        message = update.effective_message
-
-        if message.chat.id != GROUP_ID:
-            return "Ignorado: outro grupo", 200
-
-        if message.text:
-            texto = message.text.strip().lower()
-
-            if texto.endswith("pix"):
-                processar_comprovante(bot, message, tipo="pix")
-            elif "x" in texto:
-                processar_comprovante(bot, message, tipo="cartao")
-            elif texto == "✅":
-                marcar_como_pago(bot, message)
-            elif texto == "total que devo":
-                total_pendentes(bot, message)
-            elif texto == "listar pendentes":
-                listar_pendentes(bot, message)
-            elif texto == "listar pagos":
-                listar_pagos(bot, message)
-            elif texto == "ajuda":
-                ajuda(bot, message)
-            elif texto == "último comprovante":
-                ultimo_comprovante(bot, message)
-            elif texto == "total geral":
-                total_geral(bot, message)
-
-        elif message.photo or message.document:
-            processar_comprovante(bot, message, tipo="imagem")
-
+        update = telegram.Update.de_json(request.get_json(force=True), bot)
+        if update.message:
+            processar_mensagem(bot, update.message)
     except Exception as e:
         logger.error(f"Erro no webhook: {e}")
-
     return "ok", 200
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
