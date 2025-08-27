e7b8bc4625a06fc85a5f057d7477f934cd0ea496
ariaugustoo <ariaugustoo@gmail.com>
2025-08-27 17:04:35 -0300

    main.py

--- main.py
+++ main.py
@@ -78,4 +78,5 @@ def registrar_handlers():
 registrar_handlers()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
