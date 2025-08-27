ccdbdac16540b47e349dd6464a9d57df00ecd92b
ariaugustoo <ariaugustoo@gmail.com>
2025-08-27 11:50:19 -0300

    main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os
 from flask import Flask, request
 from telegram import Bot, Update
-from telegram.ext import Dispatcher, MessageHandler, Filters
-from processador import processar_mensagem
+from telegram.ext import Dispatcher
+from processador import registrar_handlers
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -13,25 +13,16 @@ ADMIN_ID = int(os.getenv("ADMIN_ID"))
 
 bot = Bot(token=TOKEN)
 app = Flask(__name__)
-dispatcher = Dispatcher(bot, None, workers=0, use_context=True)
+dispatcher = Dispatcher(bot, None, workers=1, use_context=True)
 
-def processar(update, context):
-    mensagem = update.message.text
-    chat_id = update.message.chat_id
-    user_id = update.message.from_user.id
-    processar_mensagem(mensagem, bot, chat_id, user_id)
+registrar_handlers(dispatcher, GROUP_ID, ADMIN_ID)
 
-dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, processar))
-
-@app.route(f'/{TOKEN}', methods=['POST'])
+@app.route(f"/{TOKEN}", methods=["POST"])
 def webhook():
     update = Update.de_json(request.get_json(force=True), bot)
     dispatcher.process_update(update)
-    return 'ok'
+    return "OK", 200
 
-@app.route('/')
+@app.route("/")
 def index():
-    return 'Bot rodando com webhook!'
-
-if __name__ == '__main__':
-    app.run()
+    return "Bot rodando com sucesso!", 200
